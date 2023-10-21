8c8002e33854a1b54ae32e56a4ad27d8b36e4b0a
Alex Svem <alexeydoborin@gmail.com>
2023-10-21 12:52:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,2 @@
-from pydub import AudioSegment
-from vosk import KaldiRecognizer, Model, SetLogLevel
-import os
-import json
-import subprocess
-
-SetLogLevel(0)
-
-if not os.path.exists("model"):
-    print("Please download")
 
 
